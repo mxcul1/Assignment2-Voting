cc0ca94bcb7a872a19b48d85d87c0f7c8235950d
mxcul1 <30893516+mxcul1@users.noreply.github.com>
2017-08-23 20:03:06 +1000

    Add files via upload
    Added html pages - index and login. Added initialising

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const five = require('johnny-five');
 const firebase = require("firebase");
 //Initialise database connection
+
+
+
+
 firebase.initializeApp({
   databaseURL: "https://project1-1d4a2.firebaseio.com/"
 });
 const database = firebase.database();
-five.Board().on('ready', function() {
-	const CCA = 0; //clients currently available
-	const VCA = 0; //voters currently available
-	
+five.Board().on('ready', function() {	
 	const pin = 5
 	console.log('Ardiuno is active.');
 	const motion = new five.Motion(pin);
@@ -18,7 +19,7 @@ five.Board().on('ready', function() {
 	database.ref('motionSwitch').on('value', function(snapshot) {
 				motionSensorOn = snapshot.val();
 			});
-
+				
 	// Occurs when the sensor detects the start of a motion
 	motion.on("motionstart", function() {
 		if (motionSensorOn) {
@@ -33,16 +34,18 @@ five.Board().on('ready', function() {
 			motionLength = Date.now() - motionLength;
 			console.log("motionend", motionLength);
 			
-			//If motion is longer than 5 seconds, becomes a long motion, else short
-			if (motionLength > 5000) {
-				VCA = VCA+1; 
+			//If motion is longer than 5 seconds, long motion, voter is entering station
+			const threshold = 5000;
+			if (motionLength > threshold) {
+				//increment VCA
+				
 				if VCA > CCA {
 					//call function to create client and redirect to it
 					} elif VCA <= CCA {
 						//direct to error page
 						}
 				});
-				
+			//Else motion is a short motion, voter is leaaving station 
 			} else {
 				if VCA > 1 {
 					VCA = VCA - 1; 
